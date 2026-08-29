86c78a3c955f980898cb08f2343e6a2326c7d051
Shashwat Dhondyal <shashwatdhondyal812@gmail.com>
2026-08-29 18:20:55 +0530

    feat: added event creation API with validation

---
+++ server/models/Event.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    eventId: { type: String, required: true, unique: true },
+    eventType: { type: String, required: true },
+    customerId: { type: String, required: true },
+    paymentAmount: { type: Number, required: true },
+    status: { type: String, required: true },
+    errorCode: { type: String, required: false },
+    attemptNumber: { type: Number, required: true },
+    timestamp: { type: Date, required: true }
+});
+
+const Event  = mongoose.model("Event", eventSchema);
+export default Event;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
+import Event from "./models/Event.js";
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
 
 // Connect to Database
 connectDB();
@@ -16,6 +19,29 @@ app.get("/health", (req, res) => {
     status: "OK"
   });
 });
+
+
+//Post route for accepting the tranx req
+app.post("/events", async (req, res) => {
+  try {
+    const newEntry = new Event(req.body);
+    await newEntry.save();
+    res.json({
+      status: "OK",
+      message: "New tranasction saved",
+      event: newEntry
+    });
+  }
+  catch (error) {
+    res.status(400).json({
+      status: "ERROR",
+      message: error
+    })
+
+  }
+
+})
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
